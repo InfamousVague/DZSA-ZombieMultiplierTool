5d3beee9d7980f9825ff1714b7c3372d6f3e3e2b
RetroPronghorn <retropronghorn@gmail.com>
2019-09-29 17:17:52 -0400

    inital commit

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # ZombieMultiplierTool
 
 Multiply the zombie count in the `zombie_territories.xml` files in DayZ SA programatically.
+
+## Build Your Own File
+To build your own file, you'll need to run this application with flags corrispoinding to your multiplier you'd like to apply. For example to increase the spawn amount of `InfectedArmy` zombies to 2x default, you'd add the flag `-InfectedArmy=2`.
+
+`./main -InfectedGlobal=2 -InfectedArmy=3 -InfectedPolice=2`
+
+Flag Types:
+```
+InfectedArmy
+InfectedVillage
+InfectedMedic
+InfectedPolice
+InfectedReligious
+InfectedIndustrial
+InfectedFirefighter
+InfectedCity
+InfectedSolitude
+```
+
+You can also use the global multiplier flag to set a base multiplier (if you just want to 2x every type of zombie, you can set this flag and ignore the rest). For example `-InfectedGlobal=2`.
+
+## Input and Output files
+Set the `xml/zombie_territories.base.xml` to the contents of your current zombie_territories file from your server. The output file will be `xml/zombie_territories.xml` located after the program runs and modifies your xml base.
---
+++ main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+
+	"github.com/fatih/color"
+)
+
+// Territories in our file
+type Territories struct {
+	XMLName     xml.Name    `xml:"territory-type"`
+	Territories []Territory `xml:"territory"`
+}
+
+// Territory contain
+type Territory struct {
+	XMLName xml.Name `xml:"territory"`
+	Zones   []Zone   `xml:"zone"`
+	Color   string   `xml:"color,attr"`
+}
+
+// Zone event
+type Zone struct {
+	XMLName xml.Name `xml:"zone"`
+	Name    string   `xml:"name,attr"`
+	Smin    string   `xml:"smin,attr"`
+	Smax    string   `xml:"smax,attr"`
+	Dmin    string   `xml:"dmin,attr"`
+	Dmax    string   `xml:"dmax,attr"`
+	X       string   `xml:"x,attr"`
+	Z       string   `xml:"z,attr"`
+	R       string   `xml:"r,attr"`
+}
+
+// Flags available for change
+type Flags struct {
+	InfectedGlobal      *int
+	InfectedArmy        *int
+	InfectedVillage     *int
+	InfectedMedic       *int
+	InfectedPolice      *int
+	InfectedReligious   *int
+	InfectedIndustrial  *int
+	InfectedFirefighter *int
+	InfectedCity        *int
+	InfectedSolitude    *int
+}
+
+func buildMultipliedZone(multiplier int, zone Zone) Zone {
+	newZone := zone
+	// smin
+	smin, _ := strconv.Atoi(zone.Smin)
+	newZone.Smin = strconv.Itoa(smin * multiplier)
+	// smax
+	smax, _ := strconv.Atoi(zone.Smax)
+	newZone.Smax = strconv.Itoa(smax * multiplier)
+	// dmin
+	dmin, _ := strconv.Atoi(zone.Dmin)
+	newZone.Dmin = strconv.Itoa(dmin * multiplier)
+	// dmax
+	dmax, _ := strconv.Atoi(zone.Dmax)
+	newZone.Dmax = strconv.Itoa(dmax * multiplier)
+	return newZone
+}
+
+func main() {
+	// Flags
+	flags := Flags{
+		InfectedGlobal:      flag.Int("InfectedGlobal", 1, "InfectedGlobal multiplier amount (Real Number)."),
+		InfectedArmy:        flag.Int("InfectedArmy", 1, "InfectedArmy multiplier amount (Real Number)."),
+		InfectedVillage:     flag.Int("InfectedVillage", 1, "InfectedVillage multiplier amount (Real Number)."),
+		InfectedMedic:       flag.Int("InfectedMedic", 1, "InfectedMedic multiplier amount (Real Number)."),
+		InfectedPolice:      flag.Int("InfectedPolice", 1, "InfectedPolice multiplier amount (Real Number)."),
+		InfectedReligious:   flag.Int("InfectedReligious", 1, "InfectedReligious multiplier amount (Real Number)."),
+		InfectedIndustrial:  flag.Int("InfectedIndustrial", 1, "InfectedIndustrial multiplier amount (Real Number)."),
+		InfectedFirefighter: flag.Int("InfectedFirefighter", 1, "InfectedFirefighter multiplier amount (Real Number)."),
+		InfectedCity:        flag.Int("InfectedCity", 1, "InfectedCity multiplier amount (Real Number)."),
+		InfectedSolitude:    flag.Int("InfectedSolitude", 1, "InfectedSolitude multiplier amount (Real Number)."),
+	}
+
+	// Parse flags
+	flag.Parse()
+
+	fmt.Printf(`
+Multipliers:
+Global			- %v
+InfectedArmy 		- %v
+InfectedVillage 	- %v
+InfectedMedic 		- %v
+InfectedPolice 		- %v
+InfectedReligious 	- %v
+InfectedIndustrial 	- %v
+InfectedFirefighter	- %v
+InfectedCity		- %v
+InfectedSolitude	- %v`+"\n\n",
+		*flags.InfectedGlobal,
+		*flags.InfectedArmy,
+		*flags.InfectedVillage,
+		*flags.InfectedMedic,
+		*flags.InfectedPolice,
+		*flags.InfectedReligious,
+		*flags.InfectedIndustrial,
+		*flags.InfectedFirefighter,
+		*flags.InfectedCity,
+		*flags.InfectedSolitude,
+	)
+
+	// Read XML
+	zombieTerritoriesXML, err := os.Open("xml/zombie_territories.base.xml")
+	// Error reading or finding XML file
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	color.Yellow("Opened base Zombie Territories XML...")
+	// Keep file open
+	defer zombieTerritoriesXML.Close()
+
+	byteValue, _ := ioutil.ReadAll(zombieTerritoriesXML)
+
+	var territories Territories
+	xml.Unmarshal(byteValue, &territories)
+
+	// Scan territories
+	for i := 0; i < len(territories.Territories); i++ {
+		fmt.Printf("Territoriy %v found (%v zones)...\n",
+			territories.Territories[i].Color,
+			len(territories.Territories[i].Zones),
+		)
+
+		// Scan zones
+		selectedZones := territories.Territories[i].Zones
+		for j := 0; j < len(selectedZones); j++ {
+			// Activated multipliers
+			territories.Territories[i].Zones[j] = buildMultipliedZone(
+				*flags.InfectedArmy+*flags.InfectedGlobal-1,
+				selectedZones[j],
+			)
+		}
+		fmt.Printf("Multipliers applied to %v zones. \n", len(selectedZones))
+	}
+
+	file, _ := xml.MarshalIndent(territories, "", "	")
+	_ = ioutil.WriteFile("xml/zombie_territories.xml", file, 0644)
+	color.Green("\n\nWrote `zombie_territories.xml` to the `xml/` directory. Upload to your server to modify spawns")
+}
